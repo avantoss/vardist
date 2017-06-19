acf7408014aa75c4c7ce3adb1d4a2e61fd2c070e
robertzk <technoguyrob@gmail.com>
2017-06-19 09:05:28 -0500

    Full compliance with LICENSE requirements.

--- tests/test-all.R
+++ tests/test-all.R
@@ -1,3 +1,7 @@
+# Copyright 2017 Avant
+# This file is licensed under the MIT license. For a full copy of the license, see: 
+# https://github.com/avantoss/open-source/blob/master/LICENSE_MIT
+
 library("testthat")
 library("vardist")
 test_check("vardist")
